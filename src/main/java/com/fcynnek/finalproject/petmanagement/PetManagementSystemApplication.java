0b060835ad6557013ca4a9a09a831cea208aa5a1
fcynnek <kchengfung@gmail.com>
2023-08-29 19:51:54 -0400

    Initial Commit

---
+++ src/main/java/com/fcynnek/finalproject/petmanagement/PetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fcynnek.finalproject.petmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcynnek/finalproject/petmanagement/PetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcynnek.finalproject.petmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
