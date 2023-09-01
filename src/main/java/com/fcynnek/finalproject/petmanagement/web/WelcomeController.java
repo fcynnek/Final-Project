1e2a2d53628e2443a15c1906aaa4e93adde2f5f9
fcynnek <kchengfung@gmail.com>
2023-09-01 17:01:29 -0400

    Initializing controller for index template

---
+++ src/main/java/com/fcynnek/finalproject/petmanagement/web/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.fcynnek.finalproject.petmanagement.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/")
+	public String getWelcome () {
+		return "index";
+	}
+}
