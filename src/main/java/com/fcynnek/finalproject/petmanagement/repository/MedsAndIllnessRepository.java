9df3cf9ec7c460f9e22f6945fe6c2f14ef6de298
fcynnek <kchengfung@gmail.com>
2023-09-01 17:01:02 -0400

    Initializing repository for medication table

---
+++ src/main/java/com/fcynnek/finalproject/petmanagement/repository/MedsAndIllnessRepository.java
@@ -0,0 +1,11 @@
+package com.fcynnek.finalproject.petmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fcynnek.finalproject.petmanagement.domain.Medication;
+
+@Repository
+public interface MedsAndIllnessRepository extends JpaRepository<Medication, Integer> {
+
+}
