fe1f5fdddf421a9889bcaa3a67c7845293976c78
fcynnek <kchengfung@gmail.com>
2023-09-01 17:00:13 -0400

    Medication table and seed data

--- src/main/java/com/fcynnek/finalproject/petmanagement/domain/Medication.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/domain/Medication.java
@@ -1,7 +1,16 @@
 package com.fcynnek.finalproject.petmanagement.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Medication {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String illness;
 	private String description;
 	private String sideEffects;
