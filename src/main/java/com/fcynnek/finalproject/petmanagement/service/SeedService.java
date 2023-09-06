748868a354a0e744754dcaa1bfa7b78e57efa344
fcynnek <kchengfung@gmail.com>
2023-09-06 19:46:25 -0400

    Seed Service

--- src/main/java/com/fcynnek/finalproject/petmanagement/service/SeedService.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/service/SeedService.java
@@ -1,8 +1,13 @@
 package com.fcynnek.finalproject.petmanagement.service;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fcynnek.finalproject.petmanagement.domain.Medication;
 import com.fcynnek.finalproject.petmanagement.repository.MedsAndIllnessRepository;
 
 @Service
@@ -10,4 +15,31 @@ public class SeedService {
 
 	@Autowired
 	private MedsAndIllnessRepository medsRepo;
+	
+	
+	private String[] getData (String filepath) {
+		List<String> data;
+		String[] dataArray = null;
+		
+		try {
+			data = Files.readAllLines(Paths.get(filepath));
+			dataArray = data.toArray(new String[0]);
+			
+			for (String line : dataArray) {
+				System.out.println(data);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dataArray;
+	}
+	
+	
+	private void seedDataMeds () {
+		
+		List<Medication> medications = medsRepo.findAll();
+		if (medications.size() < 10) {
+			String[] meds = datafile
+		}
+	}
 }
