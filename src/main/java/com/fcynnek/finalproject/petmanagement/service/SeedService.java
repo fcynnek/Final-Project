ef0831ef9be518e86e0407fcd476a392284ea4e1
Kenny Cheng <kchengfung@gmail.com>
2023-09-07 17:35:12 -0400

    Working on seed service

--- src/main/java/com/fcynnek/finalproject/petmanagement/service/SeedService.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/service/SeedService.java
@@ -3,6 +3,7 @@ package com.fcynnek.finalproject.petmanagement.service;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,7 +40,15 @@ public class SeedService {
 		
 		List<Medication> medications = medsRepo.findAll();
 		if (medications.size() < 10) {
-			String[] meds = datafile
+			String[] meds = getData("src/main/resources/data/SeedData.txt");
+			
+			for (int i = 0; i < 100; i++) {
+				Medication medication = new Medication();
+				
+				String line = meds[(meds.length - 1)];
+				medication.setIllness(line.substring(0, line.indexOf("|")));
+				medication.setDescription(line.indexOf("|") + 1, )
+			}
 		}
 	}
 }
