e4b59304bf4d5ec1f7cdd1c7f697170a7b0b79c8
fcynnek <kchengfung@gmail.com>
2023-09-23 17:55:06 -0400

    Implementing Spring Security

--- src/main/java/com/fcynnek/finalproject/petmanagement/security/JwtAuthenticationFilter.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/security/JwtAuthenticationFilter.java
@@ -37,7 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        if (org.springframework.util.StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
