6661a97d501e69fc790352bdbde28adc8eb97edd
fcynnek <kchengfung@gmail.com>
2023-09-24 17:57:28 -0400

    Updating references

--- src/main/java/com/fcynnek/finalproject/petmanagement/security/JwtAuthenticationFilter.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/security/JwtAuthenticationFilter.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.coderscampus.SpringSecurityJWTDemo.service.UserService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
