f3c8074cce9e406c7410b2848b89ffd412b828f0
Kenny Cheng <kchengfung@gmail.com>
2023-12-27 17:32:24 -0500

    Adding screen size adaptation

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,27 +1,37 @@
-const hero = document.querySelector('.hero'); 
-const slider = document.querySelector('.slider'); 
-const logo = document.querySelector('#logo'); 
-const hamburger = document.querySelector('.hamburger'); 
-const headline = document.querySelector('.headline'); 
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.headline');
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero,1,
-          {height:"0%"}, 
-          {height:"80%", ease: Power2.easeInOut})
+tl.fromTo(hero, 1,
+        { height: "0%" },
+        { height: "80%", ease: Power2.easeInOut })
 
-.fromTo(hero, 1.2, 
-        {width: "100%"}, 
-        {width: "80%", ease: Power2.easeInOut})
+        .fromTo(hero, 1.2,
+                { width: "100%" },
+                { width: "80%", ease: Power2.easeInOut })
 
-.fromTo(slider, 1.2, 
-        {x: "-100%"}, 
-        {x:"0%", ease: Power2.easeInOut}, "-=1.2")
+        .fromTo(slider, 1.2,
+                { x: "-100%" },
+                { x: "0%", ease: Power2.easeInOut }, "-=1.2")
 
-.fromTo(logo, 0.5, 
-        { opacity: 0, x: 30 }, 
-        { opacity: 1, x: 0 }, "-=0.5")
+        .fromTo(logo, 0.5,
+                { opacity: 0, x: 30 },
+                { opacity: 1, x: 0 }, "-=0.5")
 
-.fromTo(hamburger, 0.5, 
-        { opacity: 0, x: 30 }, 
-        { opacity: 1, x: 0 }, "-=0.5");
+        .fromTo(hamburger, 0.5,
+                { opacity: 0, x: 30 },
+                { opacity: 1, x: 0 }, "-=0.5");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+        var menuButton = document.querySelector('.menu-btn');
+        var menuLinks = document.getElementById('menu-links');
+
+        menuButton.addEventListener('click', function () {
+                menuLinks.classList.toggle('show-menu');
+        });
+});
