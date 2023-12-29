262373e703483559c8b1daffb9851a61b8e6ee25
Kenny Cheng <kchengfung@gmail.com>
2023-12-28 19:39:28 -0500

    JS for login/logout button

---
+++ src/main/resources/static/button_switch.js
@@ -0,0 +1,48 @@
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function checkAuthenticationStatus() {
+  const accessToken = getCookie('accessToken');
+  const refreshToken = getCookie('refreshToken');
+
+  if (accessToken && refreshToken) {
+    updateLoginButton(true);
+  } else {
+    updateLoginButton(false);
+  }
+}
+
+function updateLoginButton(isAuthenticated) {
+  const loginButton = document.getElementById('loginButton');
+
+  if (isAuthenticated) {
+    loginButton.innerText = 'Logout';
+    loginButton.href = '/logout';
+  } else {
+    loginButton.innerText = 'Login | Register';
+    loginButton.href = '/register';
+  }
+}
+
+// Call the function to check the authentication status when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+  checkAuthenticationStatus();
+
+  // Add click event listener to the loginButton
+  const loginButton = document.getElementById('loginButton');
+  loginButton.addEventListener('click', function (event) {
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Add your logic for login or logout here
+    const isAuthenticated = loginButton.innerText === 'Logout';
+    if (isAuthenticated) {
+      window.location.href = '/logout';
+    } else {
+      window.location.href = '/register';
+    }
+  });
+});
--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -1,7 +1,9 @@
+// This function is for the toggle-slider for the login page
 document.querySelector('.img__btn').addEventListener('click', function () {
   document.querySelector('.cont').classList.toggle('s--signup');
 });
 
+// This function is to check whether the passwords entered in the registration form are the same
 document.getElementById('registrationForm').addEventListener('submit', function (event) {
   var registrationPassword = document.getElementById('registrationPassword').value;
   var confirmPassword = document.getElementById('confirmPassword').value;
