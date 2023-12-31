a26033ec1c9be7f5f218d1efb206dfd0e2275aef
fcynnek <kchengfung@gmail.com>
2023-12-30 19:30:07 -0500

    Model now displaying username on authentication

--- src/main/java/com/fcynnek/finalproject/petmanagement/web/AuthenticationController.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/web/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.fcynnek.finalproject.petmanagement.web;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -25,99 +26,115 @@ import com.fcynnek.finalproject.petmanagement.dao.response.JwtAuthenticationResp
 import com.fcynnek.finalproject.petmanagement.dao.response.TokenRefreshResponse;
 import com.fcynnek.finalproject.petmanagement.domain.RefreshToken;
 import com.fcynnek.finalproject.petmanagement.domain.User;
+import com.fcynnek.finalproject.petmanagement.repository.UserRepository;
 import com.fcynnek.finalproject.petmanagement.security.AuthenticationServiceImpl;
 import com.fcynnek.finalproject.petmanagement.security.JwtService;
 import com.fcynnek.finalproject.petmanagement.service.RefreshTokenService;
 import com.fcynnek.finalproject.petmanagement.service.UserServiceImpl;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 //@RestController
 //@RequestMapping("/api/v1/auth")
 @Controller
 public class AuthenticationController {
-    private final AuthenticationServiceImpl authenticationService;
-    private final RefreshTokenService refreshTokenService;
-    private final JwtService jwtService;
-    private final UserServiceImpl userService;
-    
-    public AuthenticationController(AuthenticationServiceImpl authenticationService,
-			RefreshTokenService refreshTokenService, JwtService jwtService, UserServiceImpl userService) {
+	private final AuthenticationServiceImpl authenticationService;
+	private final RefreshTokenService refreshTokenService;
+	private final JwtService jwtService;
+	private final UserServiceImpl userService;
+	private final UserRepository userRepo;
+
+	public AuthenticationController(AuthenticationServiceImpl authenticationService,
+			RefreshTokenService refreshTokenService, JwtService jwtService, UserServiceImpl userService,
+			UserRepository userRepo) {
 		super();
 		this.authenticationService = authenticationService;
 		this.refreshTokenService = refreshTokenService;
 		this.jwtService = jwtService;
 		this.userService = userService;
+		this.userRepo = userRepo;
 	}
-    
 
-    @GetMapping("/signin")
-	public String getLogin (@ModelAttribute("user") User user) {
+	@GetMapping("/signin")
+	public String getLogin(@ModelAttribute("user") User user) {
 		return "login";
 	}
-    
-    @PostMapping("/signin")
-    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request, @RequestBody User user) {
-    	Optional<User> existingUser = userService.findUserByEmail(user.getEmail());
-    	String accessToken = jwtService.generateToken(user);
-    	
-    	return ResponseEntity.ok(authenticationService.signin(request));
-    }
 
-    @GetMapping("/login-error")
-    public String loginError (Model model) {
-    	model.addAttribute("loginError", true);
-    	return "login";
-    }
-    
-    @GetMapping("/authenticated")
-	public String getUserDashboard (Model model, User user) {
-    	Optional<User> authenticatedUser = userService.findUserByEmail(user.getEmail());
-    	model.addAttribute("user", authenticatedUser);
-    	
-		return "user_dashboard";
+	@PostMapping("/signin")
+	public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request,
+			@RequestBody User user) {
+		Optional<User> existingUser = userService.findUserByEmail(user.getEmail());
+		String accessToken = jwtService.generateToken(user);
+
+		return ResponseEntity.ok(authenticationService.signin(request));
 	}
-    
-    @GetMapping("/status")
-    public ResponseEntity<String> getAuthenticatedStatus() {
-    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    	
-    	if (auth != null && auth.isAuthenticated()) {
-    		return ResponseEntity.ok("authenticated");
-    	} else {
-    		return ResponseEntity.ok("not_authenticated");
-    	}
-    }
-    
-    @PostMapping("/refreshtoken")
-    public ResponseEntity<?> refreshtoken(@RequestBody RefreshTokenRequest request) {
-    	String requestRefreshToken = request.refreshToken();
-    	
-    	return refreshTokenService.findByToken(requestRefreshToken)
-    			.map(refreshTokenService::verifyExpiration)
-    			.map(RefreshToken::getUser)
-    			.map(user -> {
-    				String token = jwtService.generateToken(user);
-    				return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
-    			})
-    			.orElseThrow(() -> new IllegalStateException(
-    					"Refresh token " + requestRefreshToken + " is not in database!"));
-    }
-    
-    @GetMapping("/medication") 
-    public String getMedication() {
-    	return "medication";
-    }
-    
-    
-    /*
-     * This code is from Trevor's original implementation which might be helpful for those who are not using server rendering templates
-     * 
-     * @PostMapping("/signin") public String authenticateLogin
-     * (@ModelAttribute("user") User user, SignInRequest request) { 
-     * Optional<User> existingUser = userService.findUserByEmail(user.getEmail()); 
-     * User loggedUser = ((User) userService).loadUserByUsername(user.getUsername()); 
-     * String accessToken = jwtService.generateToken(user);
-     * 
-     * return ResponseEntity.ok(authenticationService.signin(request)); }
-     */
-    
+
+	@GetMapping("/login-error")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login";
+	}
+
+	@GetMapping("/authenticated")
+	public String getUserDashboard(Model model, HttpServletRequest request) {
+//    	User user, Principal principal
+		Principal principal = request.getUserPrincipal();
+
+//    	model.addAttribute("user", authenticatedUser);
+		if (principal != null) {
+			String username = principal.getName();
+			Optional<User> authenticatedUser = userService.findUserByEmail(username);
+
+			if (authenticatedUser.isPresent()) {
+				model.addAttribute("user", authenticatedUser);
+				return "user_dashboard";
+			} else {
+				return "error";
+			}
+		} else {
+			return "error";
+		}
+	}
+
+	@GetMapping("/status")
+	public ResponseEntity<String> getAuthenticatedStatus() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if (auth != null && auth.isAuthenticated()) {
+			return ResponseEntity.ok("authenticated");
+		} else {
+			return ResponseEntity.ok("not_authenticated");
+		}
+	}
+
+	@PostMapping("/refreshtoken")
+	public ResponseEntity<?> refreshtoken(@RequestBody RefreshTokenRequest request) {
+		String requestRefreshToken = request.refreshToken();
+
+		return refreshTokenService.findByToken(requestRefreshToken).map(refreshTokenService::verifyExpiration)
+				.map(RefreshToken::getUser).map(user -> {
+					String token = jwtService.generateToken(user);
+					return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
+				}).orElseThrow(() -> new IllegalStateException(
+						"Refresh token " + requestRefreshToken + " is not in database!"));
+	}
+
+	@GetMapping("/medication")
+	public String getMedication() {
+		return "medication";
+	}
+
+	/*
+	 * This code is from Trevor's original implementation which might be helpful for
+	 * those who are not using server rendering templates
+	 * 
+	 * @PostMapping("/signin") public String authenticateLogin
+	 * (@ModelAttribute("user") User user, SignInRequest request) { Optional<User>
+	 * existingUser = userService.findUserByEmail(user.getEmail()); User loggedUser
+	 * = ((User) userService).loadUserByUsername(user.getUsername()); String
+	 * accessToken = jwtService.generateToken(user);
+	 * 
+	 * return ResponseEntity.ok(authenticationService.signin(request)); }
+	 */
+
 }
