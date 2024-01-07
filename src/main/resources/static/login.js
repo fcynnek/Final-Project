02a4f329ebd366eeec5f69a05b627144153d6c3e
fcynnek <kchengfung@gmail.com>
2024-01-06 20:19:08 -0500

    Adding error message

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -4,7 +4,7 @@ document.querySelector('.img__btn').addEventListener('click', function () {
 });
 
 // This function is to check whether the passwords entered in the registration form are the same
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
+document.getElementById('registrationForm').addEventListener('submit', async function (event) {
   var registrationPassword = document.getElementById('registrationPassword').value;
   var confirmPassword = document.getElementById('confirmPassword').value;
   var passwordMatchMessage = document.getElementById('passwordMatchMessage');
@@ -20,3 +20,4 @@ document.getElementById('registrationForm').addEventListener('submit', function 
     passwordMatchMessage.textContent = ''; // Clear previous message
   }
 });
+
