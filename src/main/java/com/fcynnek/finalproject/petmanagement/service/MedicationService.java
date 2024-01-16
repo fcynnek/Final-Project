ec937ccaebdebc54ef2feab36325629ccd9ba476
fcynnek <kchengfung@gmail.com>
2024-01-15 19:37:42 -0500

    Refactoring seed service to medication for data

--- src/main/java/com/fcynnek/finalproject/petmanagement/service/MedicationService.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/service/MedicationService.java
@@ -3,19 +3,41 @@ package com.fcynnek.finalproject.petmanagement.service;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Random;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fcynnek.finalproject.petmanagement.domain.Animal;
 import com.fcynnek.finalproject.petmanagement.domain.Medication;
+import com.fcynnek.finalproject.petmanagement.repository.AnimalRepository;
 import com.fcynnek.finalproject.petmanagement.repository.MedsAndIllnessRepository;
 
 @Service
-public class SeedService {
+public class MedicationService {
 
-	@Autowired
 	private MedsAndIllnessRepository medsRepo;
+
+
+	public MedicationService(MedsAndIllnessRepository medsRepo) {
+		super();
+		this.medsRepo = medsRepo;
+	}
+
+	public List<Medication> getAllMeds() {
+		return medsRepo.findAll();
+	}
+
+	public Medication save(Medication meds) {
+		return medsRepo.save(meds);
+	}
+
+	public Medication getByName(String name) {
+		return medsRepo.findByName(name);
+	}
+	
+	public void delete(Medication meds) {
+		medsRepo.delete(meds);
+	}
 	
 	
 	private String[] getData (String filepath) {
