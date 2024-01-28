7430569d75c8be4cad45f4fc41e9ccf371183197
fcynnek <kchengfung@gmail.com>
2024-01-28 18:06:02 -0500

    Adding custom query

--- src/main/java/com/fcynnek/finalproject/petmanagement/repository/AnimalRepository.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/repository/AnimalRepository.java
@@ -1,6 +1,8 @@
 package com.fcynnek.finalproject.petmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fcynnek.finalproject.petmanagement.domain.Animal;
@@ -8,6 +10,9 @@ import com.fcynnek.finalproject.petmanagement.domain.Animal;
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Integer>{
 
+	@Query("SELECT a FROM Animal a WHERE a.id = :petId")
+	Animal findPetById(@Param("petId") Integer petId);
+	
 	Animal findByName(String name);
 
 }
