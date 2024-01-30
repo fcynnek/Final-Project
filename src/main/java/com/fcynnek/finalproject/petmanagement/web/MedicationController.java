3f387e6d9059ee8d5387b105e43d791a50c46ded
Kenny Cheng <kchengfung@gmail.com>
2024-01-29 20:12:16 -0500

    Working on saving the medications for animals

--- src/main/java/com/fcynnek/finalproject/petmanagement/web/MedicationController.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/web/MedicationController.java
@@ -17,6 +17,7 @@ import com.fcynnek.finalproject.petmanagement.service.AnimalService;
 import com.fcynnek.finalproject.petmanagement.service.MedicationService;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -43,66 +44,87 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class MedicationController {
 
 	private AnimalService animalService;
-	private AnimalRepository petRepo;
+	private AnimalRepository animalRepo;
 	private MedicationService medicationService;
 	private MedsAndIllnessRepository medsAndIllnessRepo;
 	private Logger logger = LoggerFactory.getLogger(MedicationController.class);
-	
-	public MedicationController(AnimalService animalService, AnimalRepository petRepo,
+
+	public MedicationController(AnimalService animalService, AnimalRepository animalRepo,
 			MedicationService medicationService, MedsAndIllnessRepository medsAndIllnessRepo) {
 		super();
 		this.animalService = animalService;
-		this.petRepo = petRepo;
+		this.animalRepo = animalRepo;
 		this.medicationService = medicationService;
 		this.medsAndIllnessRepo = medsAndIllnessRepo;
 	}
-
 
 	@GetMapping("")
 	public String getPets(Model model) {
 		List<Animal> pets = animalService.getAllPets();
 		logger.info("Inside getPets method");
-		
+
 		List<Medication> meds = medicationService.getAllMeds();
-		
+
 		model.addAttribute("animals", pets);
 		model.addAttribute("medications", meds);
 		return "medication";
 	}
 
 	@PostMapping("/create")
-	public String processMedsForm(@ModelAttribute("medicationDTO") MedicationDTO medicationDTO, 
-			@RequestParam("medicationGiven") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate medicationGiven, Model model) {
-		Medication medication =  medicationService.convertDTOTOEntity(medicationDTO);
+	public String processMedsForm(@ModelAttribute("medicationDTO") MedicationDTO medicationDTO,
+			@RequestParam("medicationGiven") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate medicationGiven,
+			Model model) {
+		Medication medication = medicationService.convertDTOTOEntity(medicationDTO);
 		logger.info("Received medication: {}", medicationDTO);
-		
+
+		Animal animal = medication.getAnimal();
+		if (animal == null) {
+
+			animal = new Animal();
+			List<String> illnesses = new ArrayList<>();
+			illnesses.add(medicationDTO.getIllness());
+			animal.setIllnesses(illnesses);
+
+			medication.setAnimal(animal);
+		} else {
+			List<String> illnesses = animal.getIllnesses();
+			if (illnesses == null) {
+				illnesses = new ArrayList<>();
+			}
+			illnesses.add(medicationDTO.getIllness());
+			animal.setIllnesses(illnesses);
+		}
+
 		LocalDate medicationDue = medicationGiven.plusYears(1);
-		
 		String illness = medication.getIllness();
 		Medication fetchMedication = medicationService.getByIllness(illness);
-		
-		if (fetchMedication != null) {
-		    medication.setDescription(medication.getDescription());
-		    medication.setSideEffects(medication.getSideEffects());
-		    medication.setMedicationGiven(medicationGiven);
-		    medication.setMedicationDue(medicationDue);
+
+		if (fetchMedication != null && animal != null) {
+			medication.setIllness(illness);
+			medication.setDescription(medication.getDescription());
+			medication.setSideEffects(medication.getSideEffects());
+			medication.setMedicationGiven(medicationGiven);
+			medication.setMedicationDue(medicationDue);
+			medication.setAnimal(animal);
 		}
+		animalRepo.save(animal);
 		medicationService.save(medication);
+
 		return "redirect:/pet/medication";
 	}
-	
+
 	@GetMapping("/{id}")
 	public String showMedications(Model model, @PathVariable Integer id) {
 		Animal pet = animalService.getByPetId(id);
 		List<Medication> meds = medicationService.getAllMedsForPet(pet);
 		List<String> illnessList = medicationService.getIllnessList();
-		
+
 		model.addAttribute("medications", meds);
 		model.addAttribute("medicationDTO", new Medication());
 		model.addAttribute("illnessList", illnessList);
 		return "medication_create";
 	}
-	
+
 	@PostMapping("/delete")
 	public String deleteMedication(Medication medication) {
 		medicationService.delete(medication);
