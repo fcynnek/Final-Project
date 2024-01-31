ca674cfd09e2aee12a6502c882ec1a6fe89fd850
Kenny Cheng <kchengfung@gmail.com>
2024-01-30 20:41:27 -0500

    Adding ID field so that it can be picked up from the Controller

--- src/main/java/com/fcynnek/finalproject/petmanagement/dto/MedicationDTO.java
+++ src/main/java/com/fcynnek/finalproject/petmanagement/dto/MedicationDTO.java
@@ -6,34 +6,38 @@ import com.fcynnek.finalproject.petmanagement.domain.Medication;
 
 public class MedicationDTO {
 
+	private Integer id;
 	private String illness;
 	private LocalDate medicationGiven;
-	
-	
-	public MedicationDTO(String illness, LocalDate medicationGiven) {
-		super();
-		this.illness = illness;
-		this.medicationGiven = medicationGiven;
+
+
+	@Override
+	public String toString() {
+		return "MedicationDTO [id=" + id + ", illness=" + illness + ", medicationGiven=" + medicationGiven + "]";
 	}
 
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public String getIllness() {
 		return illness;
 	}
 
-
 	public void setIllness(String illness) {
 		this.illness = illness;
 	}
-
 
 	public LocalDate getMedicationGiven() {
 		return medicationGiven;
 	}
 
-
 	public void setMedicationGiven(LocalDate medicationGiven) {
 		this.medicationGiven = medicationGiven;
 	}
-	
 }
